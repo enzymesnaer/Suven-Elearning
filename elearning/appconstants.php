ef2d1eadc45877275e58843acb8faa01d00c0492
Pankaj Sharma <43950687+panka-z@users.noreply.github.com>
2020-03-17 06:54:26 +0000

    added initial draft of the files needed
    login.php => demo login page which gives login/register page depending on the cookies set on browser
    appconstants.php => contains all the constant related to application
    dbconfig.php => contains config related db connection 
    db_connection.php => used to make connection to the db and perform CRUD operations
    submitform.php => the page will handle the AJAX request from the main page and correspondingly checks/sets cookies 
    rememberme.php => contains method to check if the user is logged in via session or via cookies
    auth_cookie_session_validation.php => contains core functions to perform the logic o cookie generation and verification . Also all the calls to db_connection is made vai this file

---
+++ elearning/appconstants.php
@@ -0,0 +1,10 @@
+<?php
+// defining all the constants here 
+define('TABLE_USER', 'websiteusers');
+define('TABLE_TOKEN', 'user_auth_tokens');
+
+define('USERCOOKIE', 'userloggedin');
+define('COOKIEEXPIRY', (86400 * 30));
+define('USERINDEXPAGE', 'indexsub.php');
+
+?>
